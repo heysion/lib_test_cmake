3ffd4f83b864ff296315236a844e0f68f27a3a84
Heysion <Heysions@gmail.com>
2014-04-28 11:39:23 +0800

    update test

---
+++ DoseeCommon/src/testhello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "testhello.h"
+
+int hello_lib()
+{
+	printf("hello lib\n");
+return 0;
+}
---
+++ inc/testhello.h
@@ -0,0 +1,7 @@
+#ifndef TEST_HELLO_
+#define TEST_HELLO_
+#include <stdio.h>
+#include <stdlib.h>
+int hello_lib();
+
+#endif
